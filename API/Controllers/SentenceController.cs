196732080e3a25d62edeaed879efed048925cd0f
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2022-05-31 17:20:46 +0600

    API updated

---
+++ API/Controllers/SentenceController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using BLL;
+using BEL;
+
+namespace API.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class SentenceController : ApiController
+    {
+        // GET api/<controller>
+        [Route("api/Sententce/Get")]
+        [HttpGet]
+        public List<SentenceModel> Get()
+        {
+            return SentenceService.Get();
+        }
+
+
+
+        // POST api/<controller>
+        
+
+        
+
+    }
+}
---
+++ API/Controllers/WordsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using BEL;
+using BLL;
+
+namespace API.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class WordsController : ApiController
+    {
+        // GET api/<controller>
+        [Route("api/Words/Get")]
+        [HttpGet]
+        public List<WordsModel> Get()
+        {
+            return WordsService.Get();
+        }
+
+        
+
+        // POST api/<controller>
+        [Route("api/Words/Add")]
+        [HttpPost]
+        public void Add([FromBody] WordsModel words)
+        {
+            WordsService.Add(words);
+        }
+
+        
+    }
+}

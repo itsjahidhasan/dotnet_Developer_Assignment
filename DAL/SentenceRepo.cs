cea628a106155daebb049b0864393599837cfa0c
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2022-05-31 16:10:27 +0600

    DAL is Complete

---
+++ DAL/SentenceRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class SentenceRepo : IRepository<sentenceTable, int>
+    {
+        SentenceMakingAPIEntities db;
+        public SentenceRepo(SentenceMakingAPIEntities db)
+        {
+            this.db = db;
+        }
+        public void Add(sentenceTable e)
+        {
+            db.sentenceTables.Add(e);
+            db.SaveChanges();
+        }
+
+        public List<sentenceTable> Get()
+        {
+            return db.sentenceTables.ToList();
+        }
+    }
+}
---
+++ DAL/WordsRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class WordsRepo : IRepository<wordsTable, int>
+    {
+        SentenceMakingAPIEntities db;
+        public WordsRepo(SentenceMakingAPIEntities db)
+        {
+            this.db = db;
+        }
+        public void Add(wordsTable e)
+        {
+            db.wordsTables.Add(e);
+            db.SaveChanges();
+        }
+
+        public List<wordsTable> Get()
+        {
+            return db.wordsTables.ToList();
+        }
+    }
+}
